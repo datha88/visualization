03d2d0129f2c0e5b77174e4823b06304a73e123b
Vishnudatha Kanjur <datha88@users.noreply.github.com>
2021-07-04 16:57:40 +0200

    visualization task

---
+++ visualization/__init__.py
@@ -0,0 +1,1 @@
+from .visualization.widgetbackend import *
---
+++ visualization/visualization/widgetbackend/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) vishnudatha.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "widgetfrontend"
+module_version = "^0.1.0"
